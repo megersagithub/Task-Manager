e6d94623f440d75c0b30767949f8d29408952c87
megersagithub <megersa.daraje@astu.edu.et>
2022-08-27 12:03:58 +0300

    validating routing

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,19 +3,43 @@ const { json } = require("express")
 const { param } = require("../routes/tasks")
 
 
-const getAllTasks =  (req, res) => {
-    res.send('all items from the file')
+const getAllTasks =  async (req, res) => {
+    //res.send('all items from the file')
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
+    try {
+        const task = await Task.create(req.body)
     //res.send('create tasks')
    res.status(201).json({task})
   //res.json(req.body)
+        
+    } catch (error) {
+        res.status(500).json({msg:error})
+        
+    }
+    
 }
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
    // res.send('getting  tasks')
-   res.json({id:req.params.id})
+   try {
+    const {id:taskID} = req.params
+    const task = await Task.findOne({_id:taskID})
+    res.status(200).json({task})
+
+    // res.json({id:req.params.id})
+    
+   } catch (error) {
+    
+   }
+
+   
 }
 const updateTask = (req, res) => {
     res.send('updating tasks')
--- models/task.js
+++ models/task.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose')
 
 
 
+// const TaskSchema = new  mongoose.Schema({
+//   name: String, 
+//   completed:Boolean  
+// })
+ 
+//validation of schema
 const TaskSchema = new  mongoose.Schema({
-  name: String, completed:Boolean  
-})
+    name: {
+       type:String,
+       required:[true, 'must provide name'],
+       //to ignore unncessary spaces
+       trim:true,
+       maxlength:[25, 'name connot be morethan 25'] 
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }, 
+  })
 
 module.exports =  mongoose.model('Tasks', TaskSchema)
