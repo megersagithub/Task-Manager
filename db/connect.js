61cd2a569c4779f8790c4267d9df47322e39b495
megersagithub <megersa.daraje@astu.edu.et>
2022-08-27 10:22:08 +0300

    setup mongoDB

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose')
 
 const connectionString = 
-'mongodb+srv://Magi:<password>@nodeexpressprojects.nx66c2z.mongodb.net/?retryWrites=true&w=majority'
+'mongodb+srv://Magi:DariiMagiiLove@nodeexpressprojects.nx66c2z.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
+
+const connectDB = (url) =>{
+    return mongoose.connect(connectionString, {
+        //to remove multiple warnings
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    
+    })
+}
+module.exports = connectDB
 
