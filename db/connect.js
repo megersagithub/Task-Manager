494f704ea5b35a99b24a6cccb3c2f75b6f11331e
megersagithub <megersa.daraje@astu.edu.et>
2022-08-27 10:37:24 +0300

    setup .env

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
-const connectionString = 
-'mongodb+srv://Magi:DariiMagiiLove@nodeexpressprojects.nx66c2z.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
+
 
 const connectDB = (url) =>{
-    return mongoose.connect(connectionString, {
+    return mongoose.connect(url, {
         //to remove multiple warnings
         useNewUrlParser: true,
         useCreateIndex: true,
