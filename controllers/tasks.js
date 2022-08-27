d81f4f3ff9e9b0f3968da4017232d9bbab677f5f
megersagithub <megersa.daraje@astu.edu.et>
2022-08-27 11:16:57 +0300

    create new data using postman

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,17 @@
+const Task = require('../models/Task')
 const { json } = require("express")
 const { param } = require("../routes/tasks")
 
-const getAllTasks = (req, res) => {
+
+const getAllTasks =  (req, res) => {
     res.send('all items from the file')
 }
 
-const createTask = (req, res) => {
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
     //res.send('create tasks')
-  res.json(req.body)
+   res.status(201).json({task})
+  //res.json(req.body)
 }
 const getTask = (req, res) => {
    // res.send('getting  tasks')
