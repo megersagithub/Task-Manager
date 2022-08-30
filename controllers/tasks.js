051b83894677e123144e469cc52d7eff0d0c40a5
megersagithub <megersa.daraje@astu.edu.et>
2022-08-30 10:55:31 +0300

    setup rout for api

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -47,21 +47,38 @@ res.status(200).json({task})
 
    
 }
-const updateTask = (req, res) => {
-    res.send('updating tasks')
-}
 const deleteTask = async (req, res) => {
     try {
         const {id:taskID} = req.params
         const task = await Task.findOneAndDelete({_id:taskID})
         if (!task){
             return res.status(404).json({msg:`No task with id : ${taskID}`})
-           }
-           res.status(200).json({task})
+        }
+        res.status(200).json({task})
     } catch (error) {
         res.status(500).json({meg:error})
     }
     // res.send('deleting tasks')
+}
+const updateTask = async (req, res) => {
+    try {
+        const{id:taskID} = req.params;
+
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+            //third parameter to add new values
+            new: true,
+            runValidators: true,
+        })
+    
+        if (!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+
+    } catch (error) {
+        res.status(500).json({meg:error})
+    }
+    // res.send('updating tasks')
 }
 
 module.exports = {
