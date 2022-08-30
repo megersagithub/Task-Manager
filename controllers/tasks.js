2844d5c825c1b45c1d786a67eba2cdaf34d8f3f1
megersagithub <megersa.daraje@astu.edu.et>
2022-08-30 10:26:26 +0300

    update routing

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,21 +31,37 @@ const getTask = async (req, res) => {
    try {
     const {id:taskID} = req.params
     const task = await Task.findOne({_id:taskID})
-    res.status(200).json({task})
+    
+   if (!task){
+    return res.status(404).json({msg:`No task with id : ${taskID}`})
+   }
+res.status(200).json({task})
 
     // res.json({id:req.params.id})
     
    } catch (error) {
+    res.status(500).json({meg:error})
     
    }
+   
 
    
 }
 const updateTask = (req, res) => {
     res.send('updating tasks')
 }
-const deleteTask = (req, res) => {
-    res.send('deleting tasks')
+const deleteTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if (!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+           }
+           res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({meg:error})
+    }
+    // res.send('deleting tasks')
 }
 
 module.exports = {
